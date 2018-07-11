d20f492148164f403cc2765a23b93afca71945d2
Damian Linek <damianudl@gmail.com>
2018-07-11 21:01:24 +0200

    sql

--- user.sql
+++ user.sql
@@ -2,8 +2,8 @@ CREATE SCHEMA `sign-up-in` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci ;
 
 CREATE TABLE `sign-up-in`.`USERS` (
   `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(45) NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NULL,
   `created_at` DATETIME NULL,
   PRIMARY KEY (`email`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC));
