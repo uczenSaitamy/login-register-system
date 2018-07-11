3b562e1b6f2e65307653aaf9dacd53c103b81d18
Damian Linek <damianudl@gmail.com>
2018-07-11 18:23:59 +0200

    views

--- index.php
+++ index.php
@@ -7,16 +7,16 @@
  */
 require_once('views/header.html');
 ?>
-<div class="container">
-    <div class="row mt-5 mx-auto">
-        <div class="col-lg-3">
-            <a href="" class="btn btn-primary">Zaloguj się</a>
-        </div>
+    <div class="container">
+        <div class="row mt-5 mx-auto">
+            <div class="col-lg-3">
+                <a href="/signin.php" class="btn btn-primary">Sign in</a>
+            </div>
 
-        <div class="col-lg-3">
-            <a href="" class="btn btn-secondary">Zarejestruj się</a>
+            <div class="col-lg-3">
+                <a href="/signup.php" class="btn btn-secondary">Sign up</a>
+            </div>
         </div>
     </div>
-</div>
 <?php
 require_once('views/footer.html');
--- signin.php
+++ signin.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: damian
  * Date: 11.07.18
- * Time: 18:05
+ * Time: 18:10
  */
 require_once('views/header.html');
 ?>
@@ -16,28 +16,20 @@ require_once('views/header.html');
         <div class="row mt-5 mx-auto">
             <div class="col">
                 <div class="display-4">
-                    Sing up
+                    Sing in
                 </div>
                 <form>
                     <div class="form-group">
-                        <label for="Email">Email address</label>
-                        <input type="email" class="form-control" id="Email" aria-describedby="emailHelp"
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                                placeholder="Enter email">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.
                         </small>
                     </div>
                     <div class="form-group">
-                        <label for="Password1">Password</label>
-                        <input type="password" class="form-control" id="Password1" name="Password1" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <label for="Password2">Password</label>
-                        <input type="password" class="form-control" id="Password2" name="Password2" placeholder="Confirm Password">
-                    </div>
-                    <div class="form-group">
-                        <label for="Name">Name</label>
-                        <input type="text" class="form-control" id="Name" name="Name" placeholder="Enter Name">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
