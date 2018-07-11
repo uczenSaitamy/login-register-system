47ac6b4f8612ea52b23eb439e2a34e8d5b22715e
Damian Linek <damianudl@gmail.com>
2018-07-11 18:09:18 +0200

    initial

---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damian
+ * Date: 11.07.18
+ * Time: 18:05
+ */
+require_once('views/header.html');
+?>
+    <div class="container">
+        <div class="row mt-5 mx-auto">
+            <div class="col-lg-3">
+                <a href="" class="btn btn-primary">Zaloguj się</a>
+            </div>
+
+            <div class="col-lg-3">
+                <a href="" class="btn btn-secondary">Zarejestruj się</a>
+            </div>
+        </div>
+    </div>
+<?php
+require_once('views/footer.html');
