0e8b5c75d0d6bb81ab7fd78543697a39ffc78c5a
Damian <damianudl@gmail.com>
2019-07-17 23:34:50 +0200

    gitignore fix

--- views/home.php
+++
@@ -1,6 +0,0 @@
-<div class="row text-center">
-    <div class="col-lg">
-        <a href="<?php echo url('register') ?>" class="btn btn-lg btn-success text-capitalize m-4">register</a>
-        <a href="<?php echo url('login') ?>" class="btn btn-lg btn-danger text-capitalize m-4">login</a>
-    </div>
-</div>
