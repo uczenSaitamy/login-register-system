28abf41114db8620dbf0743df532b6e7c5f36480
Damian <damianudl@gmail.com>
2019-08-10 15:06:47 +0200

    btn logout

--- views/account.php
+++ views/account.php
@@ -3,3 +3,8 @@
         <p class="display-4">Welcome <span class="text-success"><?=$user->getEmail()?></span> on account page.</p>
     </div>
 </div>
+<div class="row text-center">
+<div class="col-lg">
+<a href="<?= url('logout')?>" class="btn btn-success btn-lg text-uppercase">logout</a>
+</div>
+</div>
